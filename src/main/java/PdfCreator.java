765ba71740da6cc8e2306076d3523b9f54ad410d
lsementa <lsementa@IST-APP-WL-0375>
2024-11-23 12:23:49 -0500

    Added fallback PDF creation using Dashboard template

--- src/main/java/PdfCreator.java
+++ src/main/java/PdfCreator.java
@@ -1,13 +1,14 @@
-import java.io.File;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.xml.transform.Result;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.sax.SAXResult;
+import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import org.apache.fop.apps.Fop;
 import org.apache.fop.apps.FopFactory;
@@ -20,31 +21,24 @@ import me.tongfei.progressbar.ProgressBarStyle;
 public class PdfCreator {
 
     public static void main(String[] args) {
-        // Suppress Apache FOP logging for only SEVERE messages
-        // This allows the progress bar to be displayed properly
         Logger fopLogger = Logger.getLogger("org.apache.fop");
         fopLogger.setLevel(Level.SEVERE);
 
-        // Directory containing the XML files
-        // Located in /usr/local/app/sghe/dw/dwprod/admin/xmltrees
-        String xmlDirectory = "C:\\Users\\lsementa\\Desktop\\indiv9348-xml\\";
-        // Directory where the PDFs will be saved
-        String pdfDirectory = "C:\\Users\\lsementa\\Desktop\\indiv9348\\";
-        // Location of the main XSL template
-        // Degree Advice uses 19 XSL templates and references an 'images' folder to create the PDF
-        // Located in /usr/local/app/sghe/dw/dwprod/local
-        String xsltFile = "C:\\Users\\lsementa\\Documents\\Degree Advice\\degree-advice-pdf\\xsl\\fopaudits.xsl";
-        // String that will collect all the fails and display them at the end
-        String fails = "Fails:\n";
+        // Paths
+        String xmlDirectory = "C:\\Users\\lsementa\\Desktop\\indiv9346-xml\\";
+        String pdfDirectory = "C:\\Users\\lsementa\\Desktop\\indiv9346\\";
 
-        // Get list of all XML files in the directory
+        // XSL
+        String xsltFile = "C:\\Users\\lsementa\\Documents\\Degree Advice\\degree-advice-pdf\\xsl\\fopaudits.xsl";
+        String xsltDashboardFile = "C:\\Users\\lsementa\\Documents\\Degree Advice\\degree-advice-pdf\\Dashboard\\DGW_Report.xsl";
+
+        String fails = "\nFails:\n";
         File dir = new File(xmlDirectory);
         File[] xmlFiles = dir.listFiles((dir1, name) -> name.endsWith(".xml"));
 
         if (xmlFiles != null && xmlFiles.length > 0) {
             int totalFiles = xmlFiles.length;
 
-            // Initialize ProgressBar
             ProgressBarBuilder builder = new ProgressBarBuilder();
             builder.setTaskName("Creating PDFs...");
             builder.setInitialMax(totalFiles);
@@ -53,56 +47,99 @@ public class PdfCreator {
             ProgressBar pb = builder.build();
 
             for (File xmlFile : xmlFiles) {
-                // Create output PDF file with the same name as the XML file, in the PDF directory
                 String outputPdf = pdfDirectory + xmlFile.getName().replace(".xml", ".pdf");
+                String outputHtml = pdfDirectory + xmlFile.getName().replace(".xml", ".html");
 
                 try {
-                    // Generate PDF for the current XML file
                     generatePDF(xmlFile.getAbsolutePath(), xsltFile, outputPdf);
-
-                    // Update progress bar
-                    pb.step();
                 } catch (Exception e) {
-                    e.printStackTrace();
-                    //System.out.println("Failed to generate PDF for: " + xmlFile.getName());
-                    fails += "Failed to generate PDF for: " + xmlFile.getName() + "\n";
+                    fails += "Primary template failed for: " + xmlFile.getName() + ". Attempting fallback with HTML...\n";
+                    try {
+                        generateHTML(xmlFile.getAbsolutePath(), xsltDashboardFile, outputHtml);
+                        try {
+                            generatePDFWithHtml(outputHtml, outputPdf);
+                            fails += "PDF created successfully with HTML.\n";
+                            // Delete the HTML file
+                            File htmlFile = new File(outputHtml);
+                            if (htmlFile.exists() && htmlFile.delete()) {
+                                fails += "Temporary HTML file deleted successfully.\n";
+                            } else {
+                                fails += "Failed to delete temporary HTML file.\n";
+                            }
+                        }catch (Exception e2) {
+                            fails += "Failed to create PDF with HTML.\n";
+                        }
+                    } catch (Exception htmlException) {
+                        fails += "Fallback HTML-to-PDF conversion failed.\n";
+                        htmlException.printStackTrace();
+                    }
                 }
+
+                pb.step();
             }
 
-            // Close the progress bar
+            // Close progress bar
             pb.close();
             // Display fails
             System.out.println(fails);
+
         } else {
             System.out.println("No XML files found in the specified directory.");
         }
     }
 
     public static void generatePDF(String xmlFile, String xsltFile, String outputPdf) throws Exception {
-        // Initialize FOP factory and set up configuration
         FopFactory fopFactory = FopFactory.newInstance(new File(".").toURI());
-
-        // Set up output stream
         OutputStream out = Files.newOutputStream(Paths.get(outputPdf));
         out = new java.io.BufferedOutputStream(out);
 
         try {
-            // Create FOP instance to generate PDF
             Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, out);
-
-            // Set up the XSLT transformer
             TransformerFactory factory = TransformerFactory.newInstance();
             Transformer transformer = factory.newTransformer(new StreamSource(new File(xsltFile)));
-
-            // Set the XML source file
             Source src = new StreamSource(new File(xmlFile));
-
-            // Create a SAXResult that will send the transformation to the FOP handler
             ContentHandler handler = fop.getDefaultHandler();
-            transformer.transform(src, new SAXResult(handler));  // Use SAXResult for FOP processing
+            transformer.transform(src, new SAXResult(handler));
         } finally {
-            // Close the output stream
             out.close();
         }
     }
+
+    public static void generateHTML(String xmlFile, String xsltFile, String outputHtml) throws Exception {
+        //System.out.println("Generating HTML for " + xmlFile + "...");
+        TransformerFactory factory = TransformerFactory.newInstance();
+        Transformer transformer = factory.newTransformer(new StreamSource(new File(xsltFile)));
+
+        // Source XML
+        Source src = new StreamSource(new File(xmlFile));
+
+        // Output HTML
+        try (OutputStream out = new BufferedOutputStream(new FileOutputStream(outputHtml))) {
+            Result result = new StreamResult(out);
+            transformer.transform(src, result);
+        }
+    }
+
+    public static void generatePDFWithHtml(String htmlFile, String outputPdf) throws Exception {
+        // Path to wkhtmltopdf executable
+        String wkhtmltopdfPath = "C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe";
+
+        // Build the process
+        ProcessBuilder processBuilder = new ProcessBuilder(
+                wkhtmltopdfPath,
+                "--enable-local-file-access",
+                htmlFile,
+                outputPdf
+        );
+
+        // Redirect error and output streams
+        processBuilder.redirectErrorStream(true);
+
+        // Start the process
+        Process process = processBuilder.start();
+
+        // Wait for the process to complete
+        int exitCode = process.waitFor();
+    }
+
 }
