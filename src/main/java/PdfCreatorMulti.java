d7f5153a4a6f8372ece674347f07617555a8999c
lsementa <lsementa@IST-APP-WL-0375>
2024-11-24 09:11:15 -0500

    Added multi-threading

--- src/main/java/PdfCreatorMulti.java
+++ src/main/java/PdfCreatorMulti.java
@@ -1,6 +1,9 @@
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.transform.Result;
@@ -10,6 +13,7 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.sax.SAXResult;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+
 import org.apache.fop.apps.Fop;
 import org.apache.fop.apps.FopFactory;
 import org.apache.fop.apps.MimeConstants;
@@ -18,21 +22,21 @@ import me.tongfei.progressbar.ProgressBar;
 import me.tongfei.progressbar.ProgressBarBuilder;
 import me.tongfei.progressbar.ProgressBarStyle;
 
-public class PdfCreator {
+public class PdfCreatorMulti {
 
     public static void main(String[] args) {
         Logger fopLogger = Logger.getLogger("org.apache.fop");
         fopLogger.setLevel(Level.SEVERE);
 
-        // Paths
-        String xmlDirectory = "C:\\Users\\lsementa\\Desktop\\indiv9348-xml\\";
-        String pdfDirectory = "C:\\Users\\lsementa\\Desktop\\indiv9348\\";
+        // Directory Paths
+        String xmlDirectory = "C:\\Users\\lsementa\\Desktop\\indiv9346-xml\\";
+        String pdfDirectory = "C:\\Users\\lsementa\\Desktop\\indiv9346\\";
 
-        // XSL
+        // XSL Templates
         String xsltFile = "C:\\Users\\lsementa\\Documents\\Degree Advice\\degree-advice-pdf\\xsl\\fopaudits.xsl";
         String xsltDashboardFile = "C:\\Users\\lsementa\\Documents\\Degree Advice\\degree-advice-pdf\\Dashboard\\DGW_Report.xsl";
 
-        String fails = "\nFails:\n";
+        StringBuilder fails = new StringBuilder();
         File dir = new File(xmlDirectory);
         File[] xmlFiles = dir.listFiles((dir1, name) -> name.endsWith(".xml"));
 
@@ -46,43 +50,70 @@ public class PdfCreator {
 
             ProgressBar pb = builder.build();
 
+            // ExecutorService that uses a dynamic thread pool size
+            int poolSize = Runtime.getRuntime().availableProcessors();
+            ExecutorService executorService = Executors.newFixedThreadPool(poolSize);
+
             for (File xmlFile : xmlFiles) {
-                String outputPdf = pdfDirectory + xmlFile.getName().replace(".xml", ".pdf");
-                String outputHtml = pdfDirectory + xmlFile.getName().replace(".xml", ".html");
+                executorService.submit(() -> {
+                    String outputPdf = pdfDirectory + xmlFile.getName().replace(".xml", ".pdf");
+                    String outputHtml = pdfDirectory + xmlFile.getName().replace(".xml", ".html");
 
-                try {
-                    generatePDF(xmlFile.getAbsolutePath(), xsltFile, outputPdf);
-                } catch (Exception e) {
-                    fails += "Primary template failed for: " + xmlFile.getName() + ". Attempting fallback with HTML...\n";
                     try {
-                        generateHTML(xmlFile.getAbsolutePath(), xsltDashboardFile, outputHtml);
+                        generatePDF(xmlFile.getAbsolutePath(), xsltFile, outputPdf);
+                    } catch (Exception e) {
+                        // Primary Template failed. Attempt fallback with HTML
                         try {
-                            generatePDFWithHtml(outputHtml, outputPdf);
-                            fails += "PDF created successfully with HTML.\n";
-                            // Delete the HTML file
-                            File htmlFile = new File(outputHtml);
-                            if (htmlFile.exists() && htmlFile.delete()) {
-                                fails += "Temporary HTML file deleted successfully.\n";
-                            } else {
-                                fails += "Failed to delete temporary HTML file.\n";
+                            // Create a temporary HTML file based on Dashboard XSL template
+                            generateHTML(xmlFile.getAbsolutePath(), xsltDashboardFile, outputHtml);
+                            try {
+                                // Create the PDF using wkhtmltopdf
+                                generatePDFWithHtml(outputHtml, outputPdf);
+                                // Delete the temporary HTML file
+                                File htmlFile = new File(outputHtml);
+                                if (htmlFile.exists() && !htmlFile.delete()) {
+                                    synchronized (fails) {
+                                        fails.append("Failed to delete temporary HTML file for: ").append(xmlFile.getName()).append("\n");
+                                    }
+                                }
+                            } catch (Exception e2) {
+                                synchronized (fails) {
+                                    fails.append("Failed to create PDF with HTML for: ").append(xmlFile.getName()).append("\n");
+                                }
                             }
-                        }catch (Exception e2) {
-                            fails += "Failed to create PDF with HTML.\n";
+                        } catch (Exception htmlException) {
+                            synchronized (fails) {
+                                fails.append("Fallback HTML-to-PDF conversion failed for: ").append(xmlFile.getName()).append("\n");
+                                //htmlException.printStackTrace();
+                            }
                         }
-                    } catch (Exception htmlException) {
-                        fails += "Fallback HTML-to-PDF conversion failed.\n";
-                        htmlException.printStackTrace();
                     }
-                }
 
-                pb.step();
+                    // Update progress bar
+                    pb.step();
+                });
+            }
+
+            // Shutdown the ExecutorService and wait for tasks to complete
+            executorService.shutdown();
+            try {
+                // Wait until all tasks are completed
+                while (!executorService.awaitTermination(20, TimeUnit.MINUTES)) {
+                    System.out.println("\nWaiting for tasks to complete...");
+                }
+            } catch (InterruptedException e) {
+                // Preserve the interrupt status and force shutdown
+                Thread.currentThread().interrupt();
+                executorService.shutdownNow();
             }
 
             // Close progress bar
             pb.close();
-            // Display fails
-            System.out.println(fails);
 
+            // Display fails
+            if (fails.length() > 0) {
+                System.out.println(fails);
+            }
         } else {
             System.out.println("No XML files found in the specified directory.");
         }
@@ -91,7 +122,7 @@ public class PdfCreator {
     public static void generatePDF(String xmlFile, String xsltFile, String outputPdf) throws Exception {
         FopFactory fopFactory = FopFactory.newInstance(new File(".").toURI());
         OutputStream out = Files.newOutputStream(Paths.get(outputPdf));
-        out = new java.io.BufferedOutputStream(out);
+        out = new BufferedOutputStream(out);
 
         try {
             Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, out);
@@ -106,7 +137,6 @@ public class PdfCreator {
     }
 
     public static void generateHTML(String xmlFile, String xsltFile, String outputHtml) throws Exception {
-        //System.out.println("Generating HTML for " + xmlFile + "...");
         TransformerFactory factory = TransformerFactory.newInstance();
         Transformer transformer = factory.newTransformer(new StreamSource(new File(xsltFile)));
 
@@ -114,7 +144,7 @@ public class PdfCreator {
         Source src = new StreamSource(new File(xmlFile));
 
         // Output HTML
-        try (OutputStream out = new BufferedOutputStream(new FileOutputStream(outputHtml))) {
+        try (OutputStream out = new BufferedOutputStream(Files.newOutputStream(Paths.get(outputHtml)))) {
             Result result = new StreamResult(out);
             transformer.transform(src, result);
         }
@@ -139,7 +169,6 @@ public class PdfCreator {
         Process process = processBuilder.start();
 
         // Wait for the process to complete
-        int exitCode = process.waitFor();
+        process.waitFor();
     }
-
 }
